77b134a151d4b8fc53eb6a0fafc745d6e9e1b5a6
Hansol Son <60804796+HansolSon1113@users.noreply.github.com>
2025-06-16 16:45:37 +0900

    Update mypage.js

--- Javascript/mypage.js
+++ Javascript/mypage.js
@@ -36,6 +36,7 @@ function getCredentials() {
 }
 
 function deleteCredentials() {
+    console.log("DE");
 	document.cookie = "id=; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/";
     document.cookie = "pw=; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/";
     location.reload();
